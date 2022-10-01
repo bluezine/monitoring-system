9bc6792e4290ea452fbf0d2450d8f5572d779e68
bluezine <bluezine0930@gmail.com>
2022-10-01 15:35:04 +0900

    MS-3  - Vue Frontend 구성

---
+++ monitoring-frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import store from "@/stores/Store";
+
+const $store = store();
+console.log($store);
+</script>
+
+<template>
+	<div class="main"></div>
+</template>
---
+++ monitoring-frontend/src/stores/Store.js
@@ -0,0 +1,7 @@
+import { defineStore } from "pinia";
+
+const store = defineStore("store", {
+  state: () => ({}),
+  actions: {},
+});
+export default store;
